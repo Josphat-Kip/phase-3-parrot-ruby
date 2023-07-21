392742f357186c2921296eed912f22c42c0df2fb
Josphat-Kip <josphatkiplimo247@gmail.com>
2023-07-21 17:39:51 +0300

    methods

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(squawk)
+    puts squawk
+    squawk
+end
+
+output = parrot("Hello, I am a parrot")
+puts "Returned phrase: " #{output}"
